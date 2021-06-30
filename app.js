67b0a6a2a42c1f1a49d3bcec6ddfec5705115eba
Vadi <tprovodnikt@gmail.com>
2021-06-30 03:10:45 +0100

    menu adequately shrinks and get to the normal size

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const navBar = document.querySelector(".nav-bar")
 const allToHide = document.querySelectorAll(".nav-to-hide")
 let navCollapsed = false;
 navCollapseButton.addEventListener('click', () => {
+    if (navCollapsed) {
+        navBar.classList.add("nav-bar-collapsed")
+    } else {
+        navBar.classList.remove("nav-bar-collapsed")
+    }
     allToHide.forEach(element => {
         if (navCollapsed) {
             element.classList.add("nav-hide")
@@ -11,13 +16,14 @@ navCollapseButton.addEventListener('click', () => {
         }
                 
     });
-    if (navCollapsed) {
-        navBar.classList.add("nav-bar-collapsed")
-        navCollapsed = false
-    } else {
-        navBar.classList.remove("nav-bar-collapsed")
-        navCollapsed = true
-    }
+    rotateCollapseButton()
 
-    
+    navCollapsed = !navCollapsed
 })
+function rotateCollapseButton() {
+    if (navCollapsed) {
+        navCollapseButton.classList.add("rotated-collapse-button")
+    } else {
+        navCollapseButton.classList.remove("rotated-collapse-button")
+    }
+}
